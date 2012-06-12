224025754a03e60c3d133fd6cef9634de9ca901f
Brad Shuttleworth <brad.shuttleworth@gmail.com>
2012-06-12 10:05:36 +0100

    more path tests; initial todo

---
+++ TODO.md
@@ -0,0 +1,6 @@
+TODO
+----
+
+* Filters are probably a bad idea: they aren't essential, and have the potential
+  to be very confusing
+* Property lists should be frozen / deepcopied.
--- test/paths.js
+++ test/paths.js
@@ -11,16 +11,16 @@
     var b = basilisk, Path = b.utils.Path;
 
     test('Basic path descent', function () {
-        var Struct = b.struct({
+        var TreeNode = b.struct({
             left: {}, right: {},
             value: {}
         }), eg1;
 
-        eg1 = new Struct({
+        eg1 = new TreeNode({
             value: '',
-            left: new Struct({
+            left: new TreeNode({
                 value: 'l',
-                right: new Struct({
+                right: new TreeNode({
                     value: 'l.r'
                 })
             })
@@ -30,26 +30,122 @@
         equal('l.r', new Path('left.right').consume(eg1).value, 'Single step path should return the immediate node');
     });
 
-    test('Deep value setting', function () {
-        var Struct = b.struct({
+    test('shallow value setting', function () {
+        var TreeNode = b.struct({
             left: {}, right: {},
             value: {}
         }), eg1, eg2;
 
-        eg1 = new Struct({
+        eg1 = new TreeNode({
             value: '',
-            left: new Struct({
+            left: new TreeNode({
                 value: 'l',
-                right: new Struct({
+                right: new TreeNode({
                     value: 'l.r'
                 })
             })
         });
 
-        eg2 = new Path('right').replace(eg1, new Struct({ value: 'new' }));
+        eg2 = new Path('right').replace(eg1, new TreeNode({ value: 'new' }));
 
         equal(eg2.right.value, 'new', 'replaced value should be at the property we expect');
         strictEqual(eg2.left, eg1.left, 'Properties that are not mentioned should be identical');
     });
 
+    test('deep value setting', function () {
+        var TreeNode = b.struct({
+            left: {}, right: {},
+            value: {}
+        }), eg1, eg2;
+
+        eg1 = new TreeNode({
+            value: '',
+            left: new TreeNode({
+                value: 'l',
+                right: new TreeNode({
+                    value: 'l.r'
+                }),
+                left: new TreeNode({
+                    value: 'l.l',
+                })
+            })
+        });
+
+        eg2 = new Path('left.right.left').replace(eg1, new TreeNode({ value: 'new' }));
+
+        equal(eg2.left.right.left.value, 'new', 'replaced value should be at the property we expect');
+        strictEqual(eg2.left.left, eg1.left.left, 'Properties that are not mentioned should be identical');
+    });
+
+    test('deep swap functions.', function () {
+        // we should be able to use a swap function approach, rather than a "set" approach.
+        var TreeNode = b.struct({
+            left: {}, right: {},
+            value: {}
+        }), eg1, eg2, eg3;
+
+        eg1 = new TreeNode({
+            value: '',
+            left: new TreeNode({
+                value: 'l',
+                right: new TreeNode({
+                    value: 'l.r'
+                }),
+                left: new TreeNode({
+                    value: 'l.l',
+                })
+            })
+        });
+
+        eg2 = new Path('left.left').swap(eg1, function (current) {
+            strictEqual(current, eg1.left.left);
+            return new TreeNode({ value: 'new' });
+        });
+
+        equal(eg2.left.left.value, 'new', 'Value should have been specified properly.');
+
+        // empty nodes might behave differently.
+
+        eg2 = new Path('right').swap(eg1, function (current) {
+            strictEqual(current, undefined, 'empty property should come through as undefined');
+            return new TreeNode({ value: 'bob' });
+        })
+
+        strictEqual(eg2.right.value, 'bob', 'null/undefined should be replaced');
+
+        eg3 = new Path('right').swap(eg2, function (current) {
+            return undefined;
+        });
+
+        strictEqual(eg3.right, undefined, 'replacement with an undefined value should actually apply that change');
+    });
+
+    test('mount points', function () {
+        var TreeNode = new b.struct({
+            left: {}, right: {},
+            value: {}
+        }), eg1, eg2, atom, mount;
+
+        eg1 = new TreeNode({
+            value: 'root',
+            left: new TreeNode({
+                value: 'left'
+            })
+        });
+
+        atom = new b.Atom(eg1);
+        mount = new b.Mount(atom, 'left');
+
+        mount.swap(function (current) {  
+            strictEqual(current, eg1.left, 'Mount point should filter received value.');
+            return current.withLeft(new TreeNode({
+                value: 'left.left'
+            }));
+        });
+
+        eg2 = mount.deref();
+
+        // first check *just* that the sub-tree is correct
+        equal(eg2.value, 'left', 'Sub-tree should still have "left" as start value');
+    });
 })();
